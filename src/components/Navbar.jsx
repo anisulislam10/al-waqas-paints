f7ac15c93be3603e7304ee818c56fc467b3458ab
Anisul Islam <anis.inbox10@gmail.com>
2025-06-27 19:15:39 +0500

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -75,12 +75,19 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           {/* Logo */}
-          <Link href="/" className="flex items-center">
-            <div className="ml-3">
-              <h1 className="text-xl font-bold text-white bg-red-700 px-1 rounded-md">Al-Waqas Paint</h1>
-              <p className="text-xs text-gray-500">Premium Coatings Solution</p>
-            </div>
-          </Link>
+       {/* Logo with text */}
+<Link href="/" className="flex items-center space-x-2">
+  {/* Logo Image */}
+  <Image 
+    src="/logoal-.png" // Replace with your actual logo path (e.g., /images/logo.png)
+    alt="Al-Waqas Paint Logo"
+    width={150}
+    height={60}
+    className="rounded-full"
+  />
+
+ 
+</Link>
 
           {/* Desktop Navigation */}
           <nav className="hidden md:flex items-center space-x-8 font-bold">
