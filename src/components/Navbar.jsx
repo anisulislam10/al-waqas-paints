7a3d03b852a2f3f297c6183399420e2a99d4453f
Anisul Islam <anis.inbox10@gmail.com>
2025-06-27 20:08:57 +0500

    updated footer and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -73,13 +73,13 @@ const Navbar = () => {
 
       {/* Main Navbar */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-20">
+        <div className="flex items-center justify-between h-15">
           {/* Logo */}
        {/* Logo with text */}
 <Link href="/" className="flex items-center space-x-2">
   {/* Logo Image */}
   <Image 
-    src="/logoal-.png" // Replace with your actual logo path (e.g., /images/logo.png)
+    src="/logoal-.png" 
     alt="Al-Waqas Paint Logo"
     width={150}
     height={60}
