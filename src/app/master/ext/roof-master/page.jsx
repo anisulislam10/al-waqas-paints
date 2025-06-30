72cf4114aa3c91e35489babbb7bf2dc409bb2974
Anisul Islam <anis.inbox10@gmail.com>
2025-06-30 23:04:46 +0500

    new

--- src/app/master/ext/roof-master/page.jsx
+++ src/app/master/ext/roof-master/page.jsx
@@ -6,7 +6,7 @@ const MasterPlastic = () => {
   const product = {
     id: 1, 
     title: "Roof Master",
-    image: "/master/11.webp", 
+    image: "/master/10.webp", 
     description: "Roof Master is a protective coat paint product of Master paints that protects and cools masonry work from any wind, leakages and seepages, while reflecting back the heat. It is specially formulated to reflect heat and shield your roof from the elements, Roof Master ensures your home stays cooler during hot months. Looking for the best roof paint price in Pakistan, get in touch with us.  Roof Master offers exceptional value, combining durability with advanced heat-reflective technology. Whether you're comparing roof coating paint prices or searching for affordable roof cooling solutions, Roof Master is your go-to choice. Buy Roof Master paint in Pakistan today and enjoy premium protection at a competitive price. Keep your roof strong and your home cool with Roof Master.",
    
     features: [
