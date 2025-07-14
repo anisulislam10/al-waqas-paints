4852d4fd0a0dab0743374a2c80cc5fc14b764ea1
Anisul Islam <anis.inbox10@gmail.com>
2025-07-14 17:03:29 +0500

    modify next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'alwaqaspaint.com' }],
+        destination: 'https://www.alwaqaspaint.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    domains: ['www.alwaqaspaint.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-Robots-Tag', value: 'index, follow' },
+          { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
+        ],
+      },
+    ];
+  },
+  trailingSlash: true,
+};
 
 export default nextConfig;
